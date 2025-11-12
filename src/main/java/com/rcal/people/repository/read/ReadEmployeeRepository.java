a29f7967390d43c0400ed86869a5021a2119ba97
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-11-12 15:43:38 -0600

    feature: test triggers

--- src/main/java/com/rcal/people/repository/read/ReadEmployeeRepository.java
+++ src/main/java/com/rcal/people/repository/read/ReadEmployeeRepository.java
@@ -4,8 +4,6 @@ import com.rcal.people.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ReadEmployeeRepository
     extends
