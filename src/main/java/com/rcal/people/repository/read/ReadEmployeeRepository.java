3454c6474f4087fc6623e7ed016c3f02e9282cb1
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-12-02 12:49:54 -0600

    hotfix: spotless apply

--- src/main/java/com/rcal/people/repository/read/ReadEmployeeRepository.java
+++ src/main/java/com/rcal/people/repository/read/ReadEmployeeRepository.java
@@ -4,7 +4,6 @@ import com.rcal.people.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface ReadEmployeeRepository
     extends
